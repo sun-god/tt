689fc22eb4092b7e8b290bbaf1aaf7a0e7dadf66
liuchangyu <liuchangyu1@sina.com>
2017-05-21 21:02:30 +0800

    r

--- src/com/atguigu/test/Test.java
+++ src/com/atguigu/test/Test.java
@@ -5,7 +5,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("你好,你叫什么名字，你来自哪里");
 		System.out.println("你好,你叫什么名字，你来自哪里");
-		System.out.println("你好,你叫什么名字，你来自哪里");
-		System.out.println("你好,你叫什么名字，你来自哪里");
+	
 	}
 }
